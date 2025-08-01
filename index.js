82a084a91a4982098d879402f5c21627ed3b5ec2
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-08-01 23:37:43 +0530

    html file me functionality added.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from "express";
 import http from "http";
 import {Server} from "socket.io";
-
 import path from "path";
 import {fileURLToPath} from "url";
 
+
 const __filename = fileURLToPath(import.meta.url);
-const _dirname = path.dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 
 const app = express();
